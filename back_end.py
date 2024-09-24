15b3263c512b8bf0fdb6261ecb506b8c253db9e3
SeyZ3 <157584578+SeyZ3@users.noreply.github.com>
2024-09-24 11:39:39 +0200

    Update back_end.py
    Gangning og deling

--- back_end.py
+++ back_end.py
@@ -12,6 +12,19 @@ def trekkFra():
     print(f'{tall2} - {tall2} = {diff}')
 
 
+def ganing():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    diff = int(tall1) * int(tall2)
+    print(f'{tall2} * {tall2} = {diff}')
+
+def deling():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    diff = int(tall1) / int(tall2)
+    print(f'{tall2} / {tall2} = {diff}')
+
+
 ''' Her er noen påbegynte tanker til utvidelse
 def gange():
     produkt = 3*3
